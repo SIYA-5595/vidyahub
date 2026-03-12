bf633e5569cfb3d9a5c58d6f558e1340c0bbf5d1
SIYA-559 <rbenisiya@gmail.com>
2026-03-12 11:36:45 +0530

    Fix package.json merge conflict

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,23 @@
-<<<<<<< HEAD
-import { defineConfig } from "eslint/config";
-=======
 import { defineConfig, globalIgnores } from "eslint/config";
->>>>>>> 7b5adfad5317e2e395ba8d84302ecc9d67bc1901
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-<<<<<<< HEAD
-  {
-    ignores: [
-      ".next/**",
-      "out/**",
-      "build/**",
-      ".firebase/**",
-      "next-env.d.ts",
-      "dataconnect-generated/**",
-      "playwright-report/**",
-      "test-results/**",
-      "blob-report/**",
-      "functions/**",
-      "*.js",
-    ],
-  },
   ...nextVitals,
   ...nextTs,
-=======
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
+    ".firebase/**",
     "next-env.d.ts",
+    "dataconnect-generated/**",
+    "playwright-report/**",
+    "test-results/**",
+    "blob-report/**",
+    "functions/**",
+    "*.js",
   ]),
->>>>>>> 7b5adfad5317e2e395ba8d84302ecc9d67bc1901
 ]);
 
 export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
   reactStrictMode: true,
-=======
->>>>>>> 7b5adfad5317e2e395ba8d84302ecc9d67bc1901
   /* config options here */
 };
 
